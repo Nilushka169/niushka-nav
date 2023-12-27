5429d656d99b52fc263ec1afc232836fa6a2ea2e
Nilushka169 <s22009961@ousl.lk>
2023-12-27 14:06:27 +0530

    navbar thw tikai

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className='navbar-container'>
-        <div className="logo">
-            <img src="./logo.png" alt=""/>
-        </div>
-    </div>
-  )
+    <nav className="navbar-container">
+      <div className="logo">
+        <img src="./logo.png" alt="" />
+        <h2>
+          PURE<span>-X</span>
+        </h2>
+      </div>
+      <div className="navbarLinks">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Our Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
